161296cead02885e453e4b73ea1d5f4027647b15
ironyhouse <ironylait@gmail.com>
2020-02-17 19:54:11 +0300

    feat: finished dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let teamName = [];
+  if (!Array.isArray(members) ) {return false}
+  members = members.filter(val => typeof val === 'string');
+  
+  for (let i = 0; i < members.length; i++) {
+    members[i] = members[i].trim();
+    teamName.push(members[i][0])
+  }
+  // for should handle non-standard cases
+  return teamName.join('').toUpperCase().split('').sort().join('');
 };
