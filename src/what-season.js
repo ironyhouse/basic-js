a955cf7fc1038332a450bc4525cc56ba3a5a8b4a
ironyhouse <ironylait@gmail.com>
2020-02-18 00:00:14 +0300

    feat: finished what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,19 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (Object.entries(date).length > 0) {
+    throw new Error();
+  }
+
+  month = date.getMonth();
+  if (0 <= month && month < 2 || month == 11) {
+    return 'winter';
+  } else if (2 <= month && month < 5) {
+    return 'spring';
+  } else if (5 <= month && month < 8) {
+    return 'summer';
+  } else if (8 <= month && month < 11) {
+    return 'autumn';
+  }
 };
