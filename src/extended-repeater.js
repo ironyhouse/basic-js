89c999f0c0eeeeee24bf008485f2f8cd758ad7f9
ironyhouse <ironylait@gmail.com>
2020-02-18 13:56:00 +0300

    feat: finished extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,31 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (options.repeatTimes === undefined) {
+        options.repeatTimes = 1;
+    }
+    if (options.separator === undefined) {
+        options.separator = '+';
+    }
+    if (options.addition === undefined) {
+        options.additionRepeatTimes = 0;
+    }
+    if (options.additionRepeatTimes === undefined) {
+        options.additionRepeatTimes = 1;
+    }
+
+    let string = '';
+    for (let i = 0; i < options.repeatTimes; i++) {
+        string += str;
+
+        for (let j = 0; j < options.additionRepeatTimes; j++) {
+            string += options.addition;
+
+            if (j + 1 !== options.additionRepeatTimes)
+                string += options.additionSeparator;
+        }
+
+        if (i + 1 !== options.repeatTimes)
+            string += options.separator;
+    }
+    return string;
 };
   
